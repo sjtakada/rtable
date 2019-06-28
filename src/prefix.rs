89c758deb630cdb1960ab484fae3f32290e97e0a
Toshiaki Takada <toshiaki.takada@gmail.com>
2019-06-28 11:59:06 -0700

    Add IPv6 tests.

--- src/prefix.rs
+++ src/prefix.rs
@@ -12,18 +12,9 @@ use std::str::FromStr;
 use std::error::Error;
 use std::fmt;
     
+// Trait to enhance IpAddr::*.
 pub trait AddressLen {
     fn address_len() -> u8;
-}
-
-// IP Prefix.
-#[derive(Debug)]
-struct Prefix<T> {
-    // IP Address.
-    address: T,
-
-    // Prefix Length.
-    len: u8,
 }
 
 impl AddressLen for Ipv4Addr {
@@ -38,6 +29,17 @@ impl AddressLen for Ipv6Addr {
     }
 }
 
+// IP Prefix.
+#[derive(Debug)]
+struct Prefix<T> {
+    // IP Address.
+    address: T,
+
+    // Prefix Length.
+    len: u8,
+}
+
+// Abstract IPv4 and IPv6 both.
 impl<T: AddressLen + FromStr> Prefix<T> {
     pub fn from_str(s: &str) -> Result<Prefix<T>, PrefixParseError> {
         let (pos, prefix_len) = match s.find('/') {
@@ -62,6 +64,10 @@ impl<T: AddressLen + FromStr> Prefix<T> {
             Err(_) => Err(PrefixParseError(())),
         }
     }
+
+    pub fn address(&self) -> &T {
+        &self.address
+    }
 }
 
 impl<T: AddressLen + ToString> fmt::Display for Prefix<T> {
@@ -85,7 +91,6 @@ impl Error for PrefixParseError {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -93,12 +98,30 @@ mod tests {
     #[test]
     pub fn test_prefix_ipv4() {
         let p = Prefix::<Ipv4Addr>::from_str("10.10.10.0/24").unwrap();
+        assert_eq!(p.address().octets(), [10, 10, 10, 0]);
         assert_eq!(p.to_string(), "10.10.10.0/24");
 
-        let p = Prefix::<Ipv4Addr>::from_str("10.10.10.10").unwrap();
-        assert_eq!(p.to_string(), "10.10.10.10/32");
+        let p = Prefix::<Ipv4Addr>::from_str("1.2.3.4").unwrap();
+        assert_eq!(p.address().octets(), [1, 2, 3, 4]);
+        assert_eq!(p.to_string(), "1.2.3.4/32");
 
         match Prefix::<Ipv4Addr>::from_str("10.10.10.10/33") {
+            Ok(_) => assert!(false, "Should return error"),
+            Err(err) => { }
+        }
+    }
+
+    #[test]
+    pub fn test_prefix_ipv6() {
+        let p = Prefix::<Ipv6Addr>::from_str("::/0").unwrap();
+        assert_eq!(p.address().segments(), [0, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(p.to_string(), "::/0");
+
+        let p = Prefix::<Ipv6Addr>::from_str("2001:1234::/48").unwrap();
+        assert_eq!(p.address().segments(), [0x2001, 0x1234, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(p.to_string(), "2001:1234::/48");
+
+        match Prefix::<Ipv6Addr>::from_str("2001:1234::/130") {
             Ok(_) => assert!(false, "Should return error"),
             Err(err) => { }
         }
