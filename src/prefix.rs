dcb69da022eee485cb37c25ce3d27e5eb721df58
Toshiaki Takada <toshiaki.takada@gmail.com>
2019-06-28 11:35:37 -0700

    Abstract IPv4/v6 address together.

--- src/prefix.rs
+++ src/prefix.rs
@@ -2,8 +2,9 @@
 // Routing Table
 //   Copyright (C) 2019 Toshiaki Takada
 //
+// IP Prefix - abstract IPv? address and prefix length.
+//
 
-//use std::net::IpAddr;
 use std::net::Ipv4Addr;
 use std::net::Ipv6Addr;
 use std::net::AddrParseError;
@@ -11,9 +12,6 @@ use std::str::FromStr;
 use std::error::Error;
 use std::fmt;
     
-type V4 = Ipv4Addr;
-type V6 = Ipv6Addr;
-
 pub trait AddressLen {
     fn address_len() -> u8;
 }
@@ -28,24 +26,36 @@ struct Prefix<T> {
     len: u8,
 }
 
-// IPv4 Prefix specialization.
-impl Prefix<V4> {
-    pub fn from_str(s: &str) -> Result<Prefix<V4>, PrefixParseError> {
+impl AddressLen for Ipv4Addr {
+    fn address_len() -> u8 {
+        32
+    }
+}
+
+impl AddressLen for Ipv6Addr {
+    fn address_len() -> u8 {
+        128
+    }
+}
+
+impl<T: AddressLen + FromStr> Prefix<T> {
+    pub fn from_str(s: &str) -> Result<Prefix<T>, PrefixParseError> {
         let (pos, prefix_len) = match s.find('/') {
             // Address with prefix length.
             Some(pos) => {
                 match s[pos + 1..].parse::<u8>() {
-                    Ok(prefix_len) if prefix_len <= 32 => (pos, prefix_len),
+                    Ok(prefix_len) if prefix_len <= T::address_len() => (pos, prefix_len),
                     _ => return Err(PrefixParseError(())),
                 }
             },
-            None => (s.len(), 32),
+            // Consider host address.
+            None => (s.len(), T::address_len()),
         };
                     
         let address_str = &s[..pos];
-        match V4::from_str(address_str) {
+        match T::from_str(address_str) {
             Ok(address) =>
-                Ok(Prefix {
+                Ok(Prefix::<T> {
                     address: address,
                     len: prefix_len,
                 }),
@@ -54,35 +64,9 @@ impl Prefix<V4> {
     }
 }
 
-impl fmt::Display for Prefix<V4> {
+impl<T: AddressLen + ToString> fmt::Display for Prefix<T> {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         write!(fmt, "{}/{}", self.address.to_string(), self.len)
-    }
-}
-
-// IPv6 Prefix specialization.
-impl Prefix<V6> {
-    pub fn from_str(s: &str) -> Result<Prefix<V6>, PrefixParseError> {
-        let (pos, prefix_len) = match s.find('/') {
-            // Address with prefix length.
-            Some(pos) => {
-                match s[pos + 1..].parse::<u8>() {
-                    Ok(prefix_len) if prefix_len <= 128 => (pos, prefix_len),
-                    _ => return Err(PrefixParseError(())),
-                }
-            },
-            None => (s.len(), 128),
-        };
-                    
-        let address_str = &s[..pos];
-        match V6::from_str(address_str) {
-            Ok(address) =>
-                Ok(Prefix {
-                    address: address,
-                    len: prefix_len,
-                }),
-            Err(_) => Err(PrefixParseError(())),
-        }
     }
 }
 
@@ -108,13 +92,13 @@ mod tests {
 
     #[test]
     pub fn test_prefix_ipv4() {
-        let p = Prefix::<V4>::from_str("10.10.10.0/24").unwrap();
+        let p = Prefix::<Ipv4Addr>::from_str("10.10.10.0/24").unwrap();
         assert_eq!(p.to_string(), "10.10.10.0/24");
 
-        let p = Prefix::<V4>::from_str("10.10.10.10").unwrap();
+        let p = Prefix::<Ipv4Addr>::from_str("10.10.10.10").unwrap();
         assert_eq!(p.to_string(), "10.10.10.10/32");
 
-        match Prefix::<V4>::from_str("10.10.10.10/33") {
+        match Prefix::<Ipv4Addr>::from_str("10.10.10.10/33") {
             Ok(_) => assert!(false, "Should return error"),
             Err(err) => { }
         }
